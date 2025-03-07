b00948d72deedbe2b84d1bc395ed8b701fa0f647
MatheusViuge <matheus.viuge05@gmail.com>
2025-03-07 20:48:08 -0300

    implement basic structure for process management

--- pipex.h
+++ pipex.h
@@ -6,17 +6,28 @@
 /*   By: mviana-v <mviana-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 15:35:23 by mviana-v          #+#    #+#             */
-/*   Updated: 2025/02/27 19:06:37 by mviana-v         ###   ########.fr       */
+/*   Updated: 2025/03/07 20:44:13 by mviana-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# include <unistd.h>
+# include <stdio.h>
+# include <stdbool.h>
+# include <sys/wait.h>
 # include "./libft/libft.h"
 
+typedef struct s_pipex
+{
+	bool	first_child;
+	int		fd_in;
+	int		fd_out;
+	int		fd[2];
+}	t_pipex;
 
-int    child_process(int *fd, char **av, char **env);
-int    parent_process(int *fd, char **av, char **env, int pid);
+void	child_process(int *fd, int ac, char **av, char **env);
+void	parent_process(int *fd, int ac, char **av, char **env, int pid);
 
 #endif
