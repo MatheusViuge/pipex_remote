7f90acb03808ec037a83ca5f9bcf452c65713d1a
MatheusViuge <matheus.viuge05@gmail.com>
2025-03-07 21:05:40 -0300

    reestruct of the mainto now print something in the terminal

--- main.c
+++ main.c
@@ -6,32 +6,33 @@
 /*   By: mviana-v <mviana-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 15:33:26 by mviana-v          #+#    #+#             */
-/*   Updated: 2025/02/27 23:50:55 by mviana-v         ###   ########.fr       */
+/*   Updated: 2025/03/07 20:39:20 by mviana-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static int	pipex( int ac, char **av, char **env)
+static void	pipex( int ac, char **av, char **env)
 {
 	int	pid;
 	int	fd[2];
 
-
+	pipe(fd);
 	pid = fork();
 	if (pid == 0)
-		child_process(fd, av, env);
+		child_process(fd, ac, av, env);
 	else if (pid == -1)
 	{
-		strerror("fork");
+		perror("fork");
 		exit (1);
 	}
 	waitpid(pid, NULL, 0);
+	parent_process(fd, ac, av, env, pid);
 }
 
 static int	here_doc(void)
 {
-	ft_putendl_fd("here_doc not implemented", 1);
+	ft_putendl_fd("here_doc not implemented", 2);
 	return (1);
 }
 
@@ -39,12 +40,15 @@ int	main(int ac, char **av, char **env)
 {
 	if (ac != 5)
 	{
-		ft_putstr_fd("Error: Wrong number of arguments\n", 2);
+		ft_putendl_fd("Error: Wrong number of arguments", 2);
+		ft_putendl_fd("Usage: ./pipex infile cmd1 cmd2 outfile",2);
 		return (1);
 	}
 	else if (ft_strncmp(av[1], "here_doc", 9) == 0)
+	{
 		if (here_doc())
 			return (1);
+	}
 	else
 		pipex(ac, av, env);
 	return (0);
